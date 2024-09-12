65153648607cd9d32d62afcc51aad0d6b31f837d
Bkoule <126973071+Bkoule@users.noreply.github.com>
2024-09-12 13:16:22 +0200

    General layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <template>
-  <div>
-
+  <div class="container">
+    <header>
+      <h1>BlueSky Explorer</h1>
+      <div>
+        <input type="text" placeholder="Rechercher un post..">
+      </div>
+    </header>
+    <main>
+      <aside>
+        <h2>Recherches</h2>
+        <button>Enregistrer la recherche</button>
+        <ul>
+          <li>Actu foot</li>
+          <li>Css ex</li>
+          <li>Vaccins</li>
+        </ul>
+      </aside>
+      <section>
+        <h2>Posts</h2>
+      </section>
+      <aside>
+        <h2>Export</h2>
+        <p>Pas de favoris</p>
+      </aside>
+    </main>
   </div>
 </template>
 
@@ -16,4 +39,18 @@ export default {
 
 <style>
 
+  .container {
+    max-width: 1200px;
+  }
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  main {
+    display: flex;
+    justify-content: space-between;
+  }
 </style>
