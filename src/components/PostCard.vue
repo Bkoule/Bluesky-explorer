30f92e92885d863c6aef0d373689f905b7f33f14
Bkoule <126973071+Bkoule@users.noreply.github.com>
2024-09-12 17:03:57 +0200

    add style in PostCard

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,13 +1,15 @@
 <template>
-    <div class="post-card" >
-        <h3>{{ username }}</h3>
-        <p>{{ content }}</p>
+    <div class="post-card" :class="{'selected' : isSelected}" >
         <button
             @click="$emit('toggle')"
             :class="['select-button', {'selected': isSelected }]">
-            fav 
+            {{ isSelected ? '✓' : '+' }}
         </button>
-        <span>{{ timestamp }}</span>
+        <div class="post-content" >
+            <h3 class="username" >{{ username }}</h3>
+            <p class="content" >{{ content }}</p>
+        </div>
+        <span class="timestamp" >{{ timestamp }}</span>
     </div>
 </template>
 
@@ -25,17 +27,67 @@
 
 <style scoped>
     .post-card {
-        border: 1px solid black;
-        padding: 10px;
-        border-radius: 5px;
+        background-color: #192734;
+        display: flex;
+        flex-direction: column;
+        position: relative;
+        transition: all 0.3s ease;
+        border: 1px solid #38444d;
+        padding: 15px;
+        border-radius: 12px;
+        height: 100%;
+        box-sizing: border-box;
+    }
+
+    .post-card:hover {
+        background-color: #22303c;
+    }
+
+    .post-card.selected {
+        border-color: #1DA1F2
     }
 
     .select-button {
+        position: absolute;
+        top: 12px;
+        right: 12px;
+        width: 30px;
+        height: 30px;
+        border-radius: 50%;
+        border: none;
+        background-color: #1DA1F2;
+        color: white;
+        font-size: 18px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
         cursor: pointer;
     }
 
-    .select-button.selected {
-        background-color: blueviolet;
+    .select-button:hover {
+        background-color: #1991db;
+    }
+
+    .post-content {
+        margin-right: 40px;
+    }
+
+    .username {
+        color: white;
+        font-size: 15px;
+        font-weight: bold;
+    }
+
+    .content {
+        color: white;
+        font-size: 13px;
+    }
+
+    .timestamp {
+        color: #8899A6;
+        font-size: 12px;
+        align-self: flex-start;
+        margin-top: auto;
     }
 
 </style>
