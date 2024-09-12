92e1af18dceae2c39cacf9a0867c09468cdba612
Bkoule <126973071+Bkoule@users.noreply.github.com>
2024-09-12 13:30:36 +0200

    add PostCard in App.vue

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,13 @@
       </aside>
       <section>
         <h2>Posts</h2>
+        <PostCard
+          v-for="post in posts"
+          :key="post.id"
+          :username="post.username"
+          :content="post.content"
+          :timestamp="post.timestamp"
+          />
       </section>
       <aside>
         <h2>Export</h2>
@@ -28,12 +35,24 @@
 </template>
 
 <script>
+import PostCard from './components/PostCard.vue';
+
 
 export default {
   name: 'App',
   components: {
-  
+    PostCard
+  },
+
+  data() {
+    return {
+      posts: [
+        {id: 1, username: 'user 1', content: 'Post 1', timestamp: 'il y a 1 heure'},
+        {id: 1, username: 'user 2', content: 'Post 2', timestamp: 'il y a 1 heure'}
+      ]
+    }
   }
+
 }
 </script>
 
