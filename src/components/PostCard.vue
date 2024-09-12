203efb80d9d68c0bc7300b321c548489428f0b4c
Bkoule <126973071+Bkoule@users.noreply.github.com>
2024-09-12 13:21:41 +0200

    Postcard component creation

---
+++ src/components/PostCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="post-card" >
+        <h3>{{ username }}</h3>
+        <p>{{ content }}</p>
+        <span>{{ timestamp }}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PostCard',
+        props: {
+            username: String,
+            content: String,
+            timestamp: String
+        }
+    }
+</script>
+
+<style>
+
+</style>
